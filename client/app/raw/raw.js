c646fc69dae5cf60236afd02e6e4ad33e06727a9
sebastian <sebastian.fondra@gmail.com>
2015-08-31 15:24:38 -0300

    antes de mergear marketer

--- client/app/raw/raw.js
+++ client/app/raw/raw.js
@@ -12,52 +12,16 @@
 		});
 	});
 
-	// Raw.factory('viewRawItem', function viewItemFactory ($modal, entityService) {
-	// 	return function viewItem (item) {
-	// 		entityService.getCrudFor('raw').get(item._id)
-	// 			.then(function (response) {
-	// 		    	$modal.open({
-	// 				    templateUrl: 'app/raw/view.html',
-	// 				    controller: function () {
-	// 				    	var viewController = this;
-	// 				    	viewController.item = response;
-	// 				    },
-	// 				    controllerAs: 'viewController'
-	// 			    });
-	// 			});
-	//     };
-	// });
-	
 	Raw.controller('RawController',
 		function ($scope, createTable, DTColumnBuilder, entityService) {
 
 		var controller = this,
 			service = entityService.getCrudFor('raw');
 
-		// function rowCallback(nRow, aData) {
-	 //        // Unbind first in order to avoid any duplicate handler (see https://github.com/l-lin/angular-datatables/issues/87)
-	 //        $('td', nRow).unbind('click');
-	 //        $('td', nRow).bind('click', function() {
-	 //            $scope.$apply(function() {
-	 //                viewRawItem(aData);
-	 //            });
-	 //        });
-	 //        return nRow;
-	 //    }
-
 		controller.dtOptions = createTable({
 			service: service,
 			viewTemplateUrl: 'app/raw/view.html'
 		});
-
-		// DTOptionsBuilder
-		// 	.fromFnPromise(function() {
-	 //        	return service.getAll();
-	 //    	})
-  //           .withOption('searching', false)
-  //           .withOption('bLengthChange', false)
-  //           .withOption('pageLength', 20)
-	 //    	.withOption('rowCallback', rowCallback);
 
 	    controller.dtColumns = [
 	        DTColumnBuilder.newColumn('Raw').withTitle('Raw'),
