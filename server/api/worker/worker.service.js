14c7be3f50736017c0081a5260783972dbc8208a
maticaputti <matias.caputti@gmail.com>
2015-09-01 12:49:43 -0300

    Add worker attribute

--- server/api/worker/worker.service.js
+++ server/api/worker/worker.service.js
@@ -1,18 +1,18 @@
 'use strict';
 
 var Worker = require('./worker.model');
-var WorkersKnowl = require('./WorkerKnowl.model');
+var WorkerKnowl = require('./WorkerKnowl.model');
 var operations = {};
 
 operations.getAll = function () {
     return Worker.find()
-        .populate('IdWorkersKnowl')
+        //.populate('IdWorkersKnowl')
         .lean().exec();
 }
 
 operations.getDetail = function (workerId) {
     return Worker.findById(workerId)
-        .populate('IdWorkersKnowl')
+        //.populate('IdWorkersKnowl')
         .lean().exec();
 }
 
