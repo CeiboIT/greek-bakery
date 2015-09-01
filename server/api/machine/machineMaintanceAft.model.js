161666607ff9535ab16ac66d193d15b30e57babc
maticaputti <matias.caputti@gmail.com>
2015-09-01 09:21:45 -0300

    Refactor machine / maintAft / maintPr / maint

--- server/api/machine/machineMaintanceAft.model.js
+++ server/api/machine/machineMaintanceAft.model.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var populatePlugin = require('mongoose-power-populate')(mongoose);
 var Schema = mongoose.Schema;
 
 var machineMaintanceAft = new Schema({
@@ -8,6 +9,15 @@ var machineMaintanceAft = new Schema({
 	'IdMaintanceDate': Date,
 	'IdMachine': { type: Schema.Types.String, ref: 'Machine' },
 	'IdMaintanceAfter': { type: Schema.Types.String, ref: 'Maintance' }
+});
+
+machineMaintanceAft.plugin(populatePlugin, {
+    IdMaintanceAfter: {
+        ref: 'Maintance',
+        foreignKey: 'IdMaintance',
+        localKey: 'IdMaintanceAfter',
+        singular: true
+    }
 });
 
 module.exports = mongoose.model('MachineMaintanceAft', machineMaintanceAft);
