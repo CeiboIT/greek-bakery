00bb16c7f5a2934d5cce16ce809eefff9ce74a1c
maticaputti <matias.caputti@gmail.com>
2015-09-01 10:10:05 -0300

    Refactor marketer

--- server/api/marketer/marketer.service.js
+++ server/api/marketer/marketer.service.js
@@ -8,114 +8,24 @@ var StoreSale = require('../store/storeSale.model');
 var SalesDate = require('./salesDate.model');
 var DromologioPelates = require('./dromologioPelates.model');
 var Dromologio = require('./dromologio.model');
-
+var IDSort = require('../sort/sort.model');
 var operations = {};
 
-function populate (marketer) {
-    return MarketerCategory.findOne().exec()
-            .then(function (category) {
-                marketer.category = category;
-                return marketer;
-            }, function (error) {
-                console.error(error);
-            });
+operations.getAll = function () {
+    return Marketer.find()
+        .populate('IDMarketerCategory storeControl sortSupliers.IDSort storeSales.IDSort salesDates dromologioPelates.IDDromologio')
+        .lean().exec();
 }
-
-operations.getStoreControlByMarketer = function (marketerId) {
-    return StoreControl.find({'IDMarketer': marketerId}).exec();
-};
-
-operations.getSortSuplierByMarketer = function (marketerId) {
-    return SortSuplier.find({'IDMarketer': marketerId}).exec();
-};
-
-operations.getStoreSaleByMarketer = function (marketerId) {
-    return StoreSale.find({'IDMarketer': marketerId}).exec();
-};
-
-operations.getSalesDateByMarketer = function (marketerId) {
-    return SalesDate.find({'IDMarketer': marketerId}).exec();
-};
-
-operations.getDromologioPelatesByMarketer = function (marketerId) {
-    return DromologioPelates.find({'IDMarketer': marketerId}).exec()
-        .then(function (dromologioPelates) {
-            var dromologioPelatesObjects = [];
-            dromologioPelates.forEach(function (dromologioPelate) {
-                var dromologioPelateObject = dromologioPelate.toObject();
-                dromologioPelateObject.IDDromologio = Dromologio.find({'IDDromologio': dromologioPelate.IDDromologio});
-                dromologioPelatesObjects.push(dromologioPelateObject);
-            });
-            return dromologioPelatesObjects;
-        })
-};
 
 operations.getDetail = function (marketerId) {
     return Marketer.findById(marketerId)
-        .then(function (marketer) {
-            var marketerObject = marketer.toObject();
-
-            if (!marketer) {
-                return null;
-            }
-
-            // add Store Controls to Marketer
-            return operations.getStoreControlByMarketer(marketer.IDMarketer)
-                .then(function (storeControls) {
-                    marketerObject.storeControl = storeControls;
-                    //console.log(marketerObject);
-                    return marketerObject;
-                })
-
-            // add Sort Supliers to Marketer
-                .then(function() {
-                    return operations.getSortSuplierByMarketer(marketer.IDMarketer)
-                        .then(function (sortSupliers) {
-                            marketerObject.sortSupliers = sortSupliers;
-                            //console.log(marketerObject);
-                            return marketerObject;
-                        })
-                })
-
-            // add Store Sales to Marketer
-                .then(function() {
-                    return operations.getStoreSaleByMarketer(marketer.IDMarketer)
-                        .then(function (storeSales) {
-                            marketerObject.storeSales = storeSales;
-                            //console.log(marketerObject);
-                            return marketerObject;
-                        })
-                })
-
-            // add Sales Dates to Marketer
-                .then(function() {
-                    return operations.getSalesDateByMarketer(marketer.IDMarketer)
-                        .then(function (salesDates) {
-                            marketerObject.salesDates = salesDates;
-                            //console.log(marketerObject);
-                            return marketerObject;
-                        })
-                })
-
-            // add Dromologio Pelates to Marketer
-                .then(function() {
-                    return operations.getDromologioPelatesByMarketer(marketer.IDMarketer)
-                        .then(function (dromologioPelates) {
-                            marketerObject.dromologioPelates = dromologioPelates;
-                            //console.log(marketerObject);
-                            return marketerObject;
-                        })
-                })
-
-            // add Marketer Category to Marketer
-                .then(function () {
-                    return populate(marketerObject)
-                        .then(function (populatedMarketer) {
-                            //console.log(populatedMarketer);
-                            return populatedMarketer;
-                        });
-                });
-        });
+        .populate('IDMarketerCategory storeControl sortSupliers.IDSort storeSales.IDSort salesDates dromologioPelates.IDDromologio')
+        .lean().exec()
+            .then (function(populate) {
+                console.log(populate.storeSales);
+            })
 }
+
+//storeControl / storeSales
 
 module.exports = operations;
--- server/api/store/storeSale.model.js
+++ server/api/store/storeSale.model.js
@@ -1,18 +1,28 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var populatePlugin = require('mongoose-power-populate')(mongoose);
 var Schema = mongoose.Schema;
 
 var storeSale = new Schema({
 	'IDStoreSales': { type: String, required: true, unique: true },
-	'IDMarketer': { type: Schema.Types.Number, ref: 'Marketer' },
+	'IDMarketer': { type: Schema.Types.String, ref: 'Marketer' },
 	'Date': Date,
 	'Parastatiko': String,
-	'IDSort': { type: Schema.Types.Number, ref: 'Sort' },
+	'IDSort': { type: Schema.Types.String, ref: 'Sort' },
 	'Price': Number,
 	'Quantity': Number,
 	'Returns': Number,
 	'IDNotEnough': Number
+});
+
+storeSale.plugin(populatePlugin, {
+    IDSort: {
+        ref: 'Sort',
+        foreignKey: 'IDSort',
+        localKey: 'IDSort',
+        singular: true
+    }
 });
 
 module.exports = mongoose.model('StoreSale', storeSale);
