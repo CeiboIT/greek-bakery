2951b0c3404b2f7685d4ee1c1fc218744999dc39
maticaputti <matias.caputti@gmail.com>
2015-09-02 14:05:44 -0300

    Add dropdown menu with entities

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,10 +2,60 @@
 
 angular.module('bakeryApp')
   .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+    $scope.menu = [
+      {
+        title: 'Home',
+        link: '/'
+      },
+      {
+        title: 'aFash',
+        link: '/aFash'
+      },
+      {
+        title: 'bFash',
+        link: '/bFash'
+      },
+      {
+        title: 'cFash',
+        link: '/cFash'
+      },
+      {
+        title: 'evaluation',
+        link: '/evaluation'
+      },
+      {
+        title: 'machine',
+        link: 'machine'
+      },
+      {
+        title: 'marketer',
+        link: 'marketer'
+      },
+      {
+        title: 'menu',
+        link: 'menu'
+      },
+      {
+        title: 'parapono',
+        link: 'parapono'
+      },
+      {
+        title: 'plant',
+        link: 'plant'
+      },
+      {
+        title: 'raws',
+        link: 'raws'
+      },
+      {
+        title: 'techInfo',
+        link: 'techInfo'
+      },
+      {
+        title: 'worker',
+        link: 'worker'
+      }
+    ];
 
     $scope.isCollapsed = true;
 
