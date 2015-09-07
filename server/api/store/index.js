02eb8b02083358711f4d09ad0ffc17bd420e423f
sebastian <sebastian.fondra@gmail.com>
2015-09-07 12:21:59 -0300

    agrego sale Control

--- server/api/store/index.js
+++ server/api/store/index.js
@@ -7,5 +7,7 @@ var router = express.Router();
 
 router.get('/sale', controller.sales.index);
 router.get('/sale/:id', controller.sales.detail);
+router.get('/control', controller.control.index);
+router.get('/control/:id', controller.control.detail);
 
 module.exports = router;
--- server/api/store/store.controller.js
+++ server/api/store/store.controller.js
@@ -3,6 +3,27 @@
 var storeService = require('./store.service');
 
 var sales = {};
+var control = {};
+
+control.index = function (req, res) {
+	storeService.control.getAll()
+    	.then(function (storeControls) {
+        	return res.status(200).json(storeControls);
+	    }, function (err) {
+	    	handleError(res, err); 
+	    });
+
+};
+
+control.detail = function (req, res) {
+	storeService.control.getDetail(req.params.id)
+		.then(function (storeControlDetail) {
+			res.json(storeControlDetail);
+		}, function (err) {
+	  		console.error(err);
+	    	handleError(res, err); 
+	    });
+};
 
 sales.index = function(req, res) {
 	storeService.sales.getAll(req, res);
@@ -17,6 +38,7 @@ sales.detail = function(req, res) {
 	    	handleError(res, err); 
 	    });
 };
+
 
 function handleError(res, err) {
     return res.status(500).send(err);
