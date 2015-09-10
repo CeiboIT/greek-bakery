0863d5113b8fc8206521f09849d89ca40ae4da30
maticaputti <matias.caputti@gmail.com>
2015-09-10 11:44:35 -0300

    Plant views

--- client/app/plant/plant.js
+++ client/app/plant/plant.js
@@ -1,42 +1,34 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	var Plant = angular.module('plant', []);
+    var Plant = angular.module('plant', []);
 
-	Plant.config(function ($stateProvider) {
-		$stateProvider.state('plant', {
-			parent: 'layout',
-			url: '/plant',
-			templateUrl: 'app/plant/list.html',
-			controller: 'PlantController as plantController'
-		});
-	});
+    Plant.config(function ($stateProvider) {
+        $stateProvider.state('plant', {
+            parent: 'layout',
+            url: '/plant',
+            templateUrl: 'app/plant/list.html',
+            controller: 'PlantController as PlantController'
+        });
+    });
 
-	Plant.factory('viewItem', function viewItemFactory ($modal, entityService) {
-		return function viewItem (item) {
-			entityService.getCrudFor('plant').get(item._id)
-				.then(function (response) {
-			    	$modal.open({
-					    templateUrl: 'app/plant/view.html',
-					    controller: function () {
-					    	var viewController = this;
-					    	viewController.item = response;
-					    },
-					    controllerAs: 'viewController'
-				    });
-				});
-	    };		
-	});
+    Plant.controller('PlantController',
+        function ($scope, createTable, DTColumnBuilder, entityService) {
 
-	Plant.controller('PlantController', 
-		function ($scope, viewItem, DTOptionsBuilder, DTColumnBuilder, entityService) {
-		
-		var controller = this,
-			service = entityService.getCrudFor('plant');
-		service.getAll()
-			.then(function (plants) {
-				controller.plant = plants[0];
-			});
-	});
+        var controller = this,
+            service = entityService.getCrudFor('plant');
+
+        controller.dtOptions = createTable({
+            service: service,
+            viewTemplateUrl: 'app/plant/view.html'
+        });
+
+        controller.dtColumns = [
+            DTColumnBuilder.newColumn('IDplant').withTitle('IDplant'),
+            DTColumnBuilder.newColumn('PlantDescription').withTitle('Plant Description'),
+            DTColumnBuilder.newColumn('EAN').withTitle('EAN')
+        ];
+
+    });
 
 }());
